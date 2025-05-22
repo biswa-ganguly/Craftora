8391dce2ca07a0925aeb9ab57912a98a12db61bb
biswa-ganguly <biswaganguly16@gmail.com>
2025-05-22 23:15:38 +0530

    `feat: updated dependencies and added functionality to Dashboard component`

--- service/GlobalApi.js
+++ service/GlobalApi.js
@@ -13,18 +13,18 @@ const axiosClient=axios.create({
 
 const CreateNewResume=(data)=>axiosClient.post('/user-resumes',data);
 
-// const GetUserResumes=(userEmail)=>axiosClient.get('/user-resumes?filters[userEmail][$eq]='+userEmail);
+const GetUserResumes=(userEmail)=>axiosClient.get('/user-resumes?filters[userEmail][$eq]='+userEmail);
 
-// const UpdateResumeDetail=(id,data)=>axiosClient.put('/user-resumes/'+id,data)
+const UpdateResumeDetail=(id,data)=>axiosClient.put('/user-resumes/'+id,data)
 
-// const GetResumeById=(id)=>axiosClient.get('/user-resumes/'+id+"?populate=*")
+const GetResumeById=(id)=>axiosClient.get('/user-resumes/'+id+"?populate=*")
 
-// const DeleteResumeById=(id)=>axiosClient.delete('/user-resumes/'+id)
+const DeleteResumeById=(id)=>axiosClient.delete('/user-resumes/'+id)
 
 export default{
     CreateNewResume,
-    // GetUserResumes,
-    // UpdateResumeDetail,
-    // GetResumeById,
-    // DeleteResumeById
+    GetUserResumes,
+    UpdateResumeDetail,
+    GetResumeById,
+    DeleteResumeById
 }
